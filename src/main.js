6329e119ff71677f8bd37070daefe58240520f40
HandsomeRed <978964494@qq.com>
2019-12-30 10:42:22 +0800

    2019年12月30日 10:40:59 在线测试系统 初次提交 学生页面(初步完成 初对接)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import qs from 'qs'
+// import vuex from 'vuex'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
+
+const baseURL = '/api'
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    contentType : "application/json;charset=utf-8"
+  }
+})
+
+axios.defaults.baseURL = '/api'
+Vue.prototype.axios = axiosInstance
 Vue.config.productionTip = false
+Vue.prototype.qs = qs
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
