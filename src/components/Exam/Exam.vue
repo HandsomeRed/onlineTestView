8dca5fa33f4ad4353e00722a0dd2c53a3caa26d9
lengthh1 <849096252@qq.com>
2020-01-02 09:30:09 +0800

    试卷列表、题库、题目列表组件

---
+++ src/components/Exam/Exam.vue
@@ -0,0 +1,129 @@
+<template>
+
+    <div id="app">
+
+        <el-table
+                :data="tableData"
+                style="width: 100%"
+                height="250">
+            <el-table-column
+                    fixed
+                    prop="test_name"
+                    label="考试名称"
+                    width="150">
+            </el-table-column>
+            <el-table-column
+                    prop="course"
+                    label="课程"
+                    width="120">
+            </el-table-column>
+            <el-table-column
+                    prop="score"
+                    label="总分"
+                    width="120">
+            </el-table-column>
+            <el-table-column
+                    prop="start_time"
+                    label="开始时间"
+                    width="210">
+            </el-table-column>
+            <el-table-column
+                    prop="release_time"
+                    label="结束时间 "
+                    width="210">
+            </el-table-column>
+            <el-table-column
+                    prop="teacher"
+                    label="创始人"
+                    width="120">
+            </el-table-column>
+
+            <el-table-column
+                    fixed="right"
+                    label="操作"
+                    width="100">
+                <template slot-scope="scope">
+                    <i class="el-icon-view" @click="showName(scope.row.test_name)"/>
+                    <i class="el-icon-delete"/>
+
+                </template>
+            </el-table-column>
+
+        </el-table>
+        <el-button type="primary" icon="el-icon-plus">创建考试</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Exam",
+        methods:{
+            showName(testname){
+                // eslint-disable-next-line no-console
+                console.log(testname)
+            }
+        },
+        data() {
+            return {
+                tableData: [{
+                    test_name: "水平测试",
+                    course: "软件工程",
+                    score: "100",
+                    start_time: "2020-01-01 20:00",
+                    release_time: "2020-01-01 22:00",
+                    teacher: "王老师",
+
+
+                },{
+                    test_name: "水平测试",
+                    course: "软件工程",
+                    score: "100",
+                    start_time: "2020-01-01 20:00",
+                    release_time: "2020-01-01 22:00",
+                    teacher: "王老师",
+
+
+                },{
+                    test_name: "水平测试",
+                    course: "软件工程",
+                    score: "100",
+                    start_time: "2020-01-01 20:00",
+                    release_time: "2020-01-01 22:00",
+                    teacher: "王老师",
+
+
+                },{
+                    test_name: "水平测试",
+                    course: "软件工程",
+                    score: "100",
+                    start_time: "2020-01-01 20:00",
+                    release_time: "2020-01-01 22:00",
+                    teacher: "王老师",
+
+
+                },{
+                    test_name: "水平测试",
+                    course: "软件工程",
+                    score: "100",
+                    start_time: "2020-01-01 20:00",
+                    release_time: "2020-01-01 22:00",
+                    teacher: "王老师",
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .el-icon-view{
+        text-align: center;
+        padding-right: 20px;
+        cursor: pointer;
+        color: #1A8CFE;
+    }
+    .el-icon-delete{
+        cursor: pointer;
+        color: #1A8CFE;
+    }
+</style>
---
+++ src/components/QuestionManager/QuestionManager.vue
@@ -0,0 +1,94 @@
+<template>
+    <div id="app">
+
+        <el-table
+                :data="tableData"
+                style="width: 100%"
+                height="500">
+            <el-table-column
+                    prop="type"
+                    label="题型"
+                    width="120">
+            </el-table-column>
+            <el-table-column
+                    prop="course"
+                    label="课程"
+                    width="150">
+            </el-table-column>
+            <el-table-column
+
+                    prop="stem"
+                    label="题目"
+                    width="500">
+            </el-table-column>
+
+            <el-table-column
+
+                    label="操作"
+                    width="120">
+                <template slot-scope="scope">
+                    <i class="el-icon-delete" @click="showName(scope.row.stem)"/>
+
+
+                </template>
+            </el-table-column>
+
+        </el-table>
+        <el-button type="primary" icon="el-icon-plus">添加题目</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "QuestionManager",
+        methods:{
+            showName(stem){
+                // eslint-disable-next-line no-console
+                console.log(stem)
+            }
+        },
+        data() {
+            return {
+                tableData: [{
+                    stem: "在机场、商厦、地铁等公共场所乘自动扶梯时应靠（）站立,另一侧供有急事赶路的人快行。",
+                    course: "社会实践",
+                    type: "单选题"
+
+
+
+                },{
+                    stem: "在参加各种社交宴请宾客中,要注意从座椅的（）侧入座,动作应轻而缓,轻松自然。",
+                    course: "软件工程",
+                    type: "单选题"
+
+
+                },{
+                    stem: "在参加各种社交宴请宾客中,要注意从座椅的（）侧入座,动作应轻而缓,轻松自然。",
+                    course: "软件工程",
+                    type: "单选题"
+
+
+                },{
+                    stem: "在参加各种社交宴请宾客中,要注意从座椅的（）侧入座,动作应轻而缓,轻松自然。",
+                    course: "软件工程",
+                    type: "单选题"
+
+                },{
+                    stem: "在参加各种社交宴请宾客中,要注意从座椅的（）侧入座,动作应轻而缓,轻松自然。",
+                    course: "软件工程",
+                    type: "单选题"
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .el-icon-delete{
+        text-align: center;
+        margin-left: 20px;
+        cursor: pointer;
+        color: #1A8CFE;
+    }
+</style>
